28b667d7c3e828c9e66096d8dfea10da1ff931fd
calibr <awcalibr@gmail.com>
2017-03-05 02:18:42 +0300

    add multiline processing closes #1

--- index.js
+++ index.js
@@ -1,24 +1,156 @@
+var EventEmitter = require("events");
+var util = require("util");
+
+function SimpleApiParser() {
+  EventEmitter.call(this);
+
+  this.inValue = false;
+  this.inEntity = false;
+  this.index = 0;
+  this.string = null;
+  this.end = false;
+  this._space = /\s/;
+}
+util.inherits(SimpleApiParser, EventEmitter);
+
+SimpleApiParser.prototype._checkUnexpectedDocEnd = function() {
+  if(this.index >= this.length) {
+    throw new Error("Unexpected document end");
+  }
+}
+
+SimpleApiParser.prototype._skipSpaces = function() {
+  for(;;) {
+    this.index++;
+    this._checkUnexpectedDocEnd();
+    if(!this._space.test(this.string[this.index])) {
+      break;
+    }
+  }
+}
+
+SimpleApiParser.prototype._readUntil = function(char) {
+  for(;;) {
+    this.index++;
+    this._checkUnexpectedDocEnd();
+    if(this.string[this.index] === char) {
+      break;
+    }
+  }
+  this.index--;
+}
+
+SimpleApiParser.prototype._getNext = function(n) {
+  if(n <= 0) {
+    return "";
+  }
+  var res = "";
+  while(n--) {
+    res += this.string[this.index];
+    this.index++;
+  }
+  return res;
+}
+
+SimpleApiParser.prototype._next = function () {
+  var char;
+  if (this.index === 0) {
+    this.emit("document_start");
+  }
+  if (this.index >= this.string.length) {
+    this.end = true;
+    return this.emit("document_end");
+  }
+  var symbol = this.string[this.index];
+  if (symbol === "<") {
+    this.index++;
+    var next7Symbols = this._getNext(7);
+    if(next7Symbols.toLowerCase() !== "!entity") {
+      throw new Error("Expected !entity, got \"" + next7Symbols + "(" + next7Symbols.toLowerCase() + ")\"");
+    }
+    // reading entity name
+    // first of skip space chars
+    this._skipSpaces();
+    // reading entity name until space
+    var entityName = "";
+    for (;;) {
+      this._checkUnexpectedDocEnd();
+      char = this.string[this.index];
+      if(this._space.test(char)) {
+        break;
+      }
+      this.index++;
+      entityName += char;
+    }
+    this.inEntity = true;
+    this.entityValueQuote = false;
+    this.emit("entity_name", entityName);
+    this._skipSpaces();
+    char = this.string[this.index];
+    var entityValueQuote = null;
+    if (char === '"' || char === "'") {
+      entityValueQuote = char;
+    }
+    var value = "";
+    var symbolEscaped = false;
+    var addChar;
+    for (;;) {
+      addChar = true;
+      this.index++;
+      this._checkUnexpectedDocEnd();
+      char = this.string[this.index];
+      if(entityValueQuote) {
+        if (!symbolEscaped && char === entityValueQuote) {
+          break;
+        }
+      } else if (this._space.test(char)) {
+        this._readUntil(">");
+        break;
+      } else if (char === ">") {
+        break;
+      }
+      if (symbolEscaped) {
+        symbolEscaped = false;
+      } else if (entityValueQuote && char === '\\') {
+        var nextSymbol = this.string[this.index + 1];
+        if(nextSymbol === entityValueQuote) {
+          symbolEscaped = true;
+          addChar = false;
+        }
+      }
+      if(addChar) {
+        value += char;
+      }
+    }
+    this.emit("entity_value", value);
+  }
+  this.index++;
+};
+
+SimpleApiParser.prototype.parse = function(str) {
+  this.string = str;
+  var i = 0;
+  while (!this._end) {
+    i++;
+    if(i > this.string.length) {
+      break;
+    }
+    this._next();
+  }
+};
+
 exports.parse = function(string) {
-  var object = {};
-  string.split(/(\n|\r\n)/).forEach(function(s) {
-    s = s.trim();
-    if(!s) {
-      return;
-    }
-    var m = s.match(/<!entity\s+(.+?)\s+(.+?)>$/i);
-    if(!m) {
-      return;
-    }
-    var key = m[1];
-    var value = m[2];
-    if(value[0] === "\"" || value[0] === "'") {
-      var quote = value[0];
-      value = value.substr(1, value.length - 2);
-      value = value.replace(new RegExp("\\\\" + quote, "g"), quote);
-    }
-    object[key] = value;
+  var parser = new SimpleApiParser();
+  var res = {};
+  var currentName;
+  parser.on("entity_name", function(name) {
+    currentName = name;
   });
-  return object;
+  parser.on("entity_value", function(value) {
+    res[currentName] = value;
+  });
+  parser.parse(string);
+  return res;
 };
 
 exports.stringify = function(object) {
